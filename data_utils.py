7e47a911411edea4818dc3523b0105b52d71958e
dadavis21 <dadavis21@yahoo.com>
2017-09-28 22:09:27 -0500

    debugging image output

--- data_utils.py
+++ data_utils.py
@@ -149,7 +149,7 @@ def get_open_images(id_data, class_limit):
         image_file = output_image_dir + '/{}.jpg'.format(image_id)
         image_bytes = BytesIO(requests.get(image_url).content)
         try:
-            image = misc.imread(image_bytes).as_type(np.uint8)
+            image = np.array(Image.open(image_bytes)).astype(np.uint8)
             torch.save({'frame':image, 'obj_vis':obj_vis}, output_image_file.format(str(image_id)))
         except:
             print('invalid')
